5d4738c2003097ac16346b1dcaab4983544740bc
afloridia <angelo.floridia@studenti.polito.it>
2023-11-24 23:48:56 +0100

    added zsh config and plugins

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,6 @@
 sudo apt-get install -y $(cat pkglist)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s $(which zsh)
+
+echo 'log out and log in again to see the results';
+
